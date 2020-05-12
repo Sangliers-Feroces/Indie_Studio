84c512f2cde19fe3891e94b4e2e01c0d684e6952
CorentinRenard <corentin.renard@epitech.eu>
2020-05-12 03:37:24 +0200

    Add CMAKE

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2020
+** OOP_indie_studio_2019
+** File description:
+** main
+*/
+
+#include "test/test.hpp"
+#include <iostream>
+#include <irrlicht/irrlicht.h>
+
+int main(void)
+{
+	std::cout << "Hellow World" << std::endl;
+	return TEST;
+}
---
+++ src/test/test.hpp
@@ -0,0 +1,10 @@
+/*
+** EPITECH PROJECT, 2020
+** OOP_indie_studio_2019
+** File description:
+** test
+*/
+
+#pragma once
+
+#define TEST 42;
