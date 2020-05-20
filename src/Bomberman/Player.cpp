55c4348e88613c99faefa7488a9ea3c026adb2bf
Adrien LENGLET <adrien.lenglet3@gmail.com>
2020-05-20 20:23:16 +0200

    Add dolphin array

--- src/Bomberman/Player.cpp
+++ src/Bomberman/Player.cpp
@@ -2,10 +2,13 @@
 
 namespace Bomberman {
 
-Player::Player(void) :
-	AnimatedMesh("res/models/dolphin.md2")
+Player::Player(void)
 {
-	setPos(irr::core::vector3df(-10, 100, 2000));
+	setPos(irr::core::vector3df(0, 0, 1000));
+	for (size_t i = 0; i < 10; i++)
+		for (size_t j = 0; j < 10; j++)
+			for (size_t k = 0; k < 10; k++)
+				add<Sub>(i * 1000, j * 1000, k * 1000);
 }
 
 Player::~Player(void)
--- src/Bomberman/Player.hpp
+++ src/Bomberman/Player.hpp
@@ -4,11 +4,22 @@
 
 namespace Bomberman {
 
-class Player : public en::AnimatedMesh
+class Player : public en::Entity
 {
 public:
 	Player(void);
 	~Player(void);
+
+	class Sub : public en::AnimatedMesh
+	{
+	public:
+		Sub(ssize_t x, ssize_t y, ssize_t z) :
+			en::AnimatedMesh("res/models/dolphin.md2")
+		{
+			setPos(irr::core::vector3df(x, y, z));
+		}
+		~Sub(void) = default;
+	};
 };
 
 }
