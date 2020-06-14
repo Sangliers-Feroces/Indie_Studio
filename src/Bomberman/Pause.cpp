225d60b1b4f7cb331254401dd8cac189663f9440
CorentinRenard <corentin.renard@epitech.eu>
2020-06-14 09:41:38 +0200

    Add Pause

--- src/Bomberman/Pause.cpp
+++ src/Bomberman/Pause.cpp
@@ -10,11 +10,28 @@
 namespace Bomberman {
 
 Pause::Pause(void) :
-	m_resume(add<Button>(irr::core::rect<irr::s32>(600, 50, 1000, 250), L"Resume")),
-	m_save(add<Button>(irr::core::rect<irr::s32>(600, 250, 1000, 450), L"Save")),
-	m_menu(add<Button>(irr::core::rect<irr::s32>(600, 450, 1000, 650), L"Return to main menu")),
-	m_leave(add<Button>(irr::core::rect<irr::s32>(600, 650, 1000, 850), L"Exit"))
+	m_background(add<Image>(session.driver.getTexture("res/GUI/pause_background.png"), irr::core::position2d<irr::s32>(500, 50))),
+	m_resume(add<Button>(irr::core::rect<irr::s32>(550, 250, 1050, 350), L"")),
+	m_save(add<Button>(irr::core::rect<irr::s32>(550, 400, 1050, 500), L"")),
+	m_menu(add<Button>(irr::core::rect<irr::s32>(550, 550, 1050, 650), L"")),
+	m_leave(add<Button>(irr::core::rect<irr::s32>(550, 700, 1050, 800), L""))
 {
+
+	m_resume.setImage(session.driver.getTexture("res/GUI/pause_resume.png"));
+	m_resume.setUseAlphaChannel(true);
+	m_resume.setIsDrawBorder(false);
+
+	m_save.setImage(session.driver.getTexture("res/GUI/pause_save.png"));
+	m_save.setUseAlphaChannel(true);
+	m_save.setIsDrawBorder(false);
+
+	m_menu.setImage(session.driver.getTexture("res/GUI/pause_menu.png"));
+	m_menu.setUseAlphaChannel(true);
+	m_menu.setIsDrawBorder(false);
+
+	m_leave.setImage(session.driver.getTexture("res/GUI/pause_exit.png"));
+	m_leave.setUseAlphaChannel(true);
+	m_leave.setIsDrawBorder(false);
 	bind(session.events.gui.button_pressed, [this](auto gui) {
 		if (m_resume == gui.Caller) {
 			session.resume_Pause = true;
